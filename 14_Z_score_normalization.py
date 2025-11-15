a0f6f176c497eec369b2b3140b86fd685a0ee7c2
Souvikmanna2005 <souvik2005110@gmail.com>
2025-11-15 12:02:14 +0530

    Scaling & Normalization file

---
+++ 14_Z_score_normalization.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+data = np.array([200,300,400,600,1000])
+
+mean = np.mean(data)
+
+std_dev = np.std(data)
+
+z_scores = (data-mean) / std_dev
+
+print('Original data:',data)
+
+print('Mean:',mean)
+
+print('Standard Deviation:',std_dev)
+
+print('Z-scores:',z_scores)
